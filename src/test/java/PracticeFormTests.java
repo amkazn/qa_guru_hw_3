390f23d64f29cf719924114a257d171162721aff
amkazn <amkazn@gmail.com>
2024-01-22 13:52:42 +0300

    to review

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -2,8 +2,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -13,7 +12,7 @@ public class PracticeFormTests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
         Configuration.timeout = 5000; // default 4000
     }
 
@@ -54,7 +53,6 @@ public class PracticeFormTests {
                 .$("#currentAddress")
                 .setValue("Baker Street 221b");
 
-
         $("#stateCity-wrapper")
                 .$("#state")
                 .click();
@@ -71,27 +69,51 @@ public class PracticeFormTests {
 
         $("#submit").click();
 
-
-//        $("#stateCity-wrapper").$("#state").setValue("NCR");
-
-//        System.out.println("********************************");
-//        System.out.println($("#subjectsInput").exists());
-//        System.out.println("********************************");
-
-
-
-
-//        $("#dateOfBirthInput");
-
-
-
-//
-//        System.out.println("********************************");
-//        System.out.println($("#dateOfBirthInput").val());
-//        System.out.println($("#dateOfBirthInput").
-//                $(byText("Choose Friday, January 26th, 2024")).exists());
-//        System.out.println($("#dateOfBirthInput").getValue());
-//        System.out.println("********************************");
-
+        $(".modal-open")
+                .shouldBe(visible);
+        $(".table-responsive")
+                .$(byText("Label"))
+                .parent()
+                .shouldHave(text("Values"));
+        $(".table-responsive")
+                .$(byText("Student Name"))
+                .parent()
+                .shouldHave(text("Sherlock Holmes"));
+        $(".table-responsive")
+                .$(byText("Student Email"))
+                .parent()
+                .shouldHave(text("s@holmes.com"));
+        $(".table-responsive")
+                .$(byText("Gender"))
+                .parent()
+                .shouldHave(text("Male"));
+        $(".table-responsive")
+                .$(byText("Mobile"))
+                .parent()
+                .shouldHave(text("1234567890"));
+        $(".table-responsive")
+                .$(byText("Date of Birth"))
+                .parent()
+                .shouldHave(text("02 June,1990"));
+        $(".table-responsive")
+                .$(byText("Subjects"))
+                .parent()
+                .shouldHave(text("Computer Science, Maths, Physics"));
+        $(".table-responsive")
+                .$(byText("Hobbies"))
+                .parent()
+                .shouldHave(text("Sports, Reading, Music"));
+        $(".table-responsive")
+                .$(byText("Picture"))
+                .parent()
+                .shouldHave(text("pic.jpg"));
+        $(".table-responsive")
+                .$(byText("Address"))
+                .parent()
+                .shouldHave(text("Baker Street 221b"));
+        $(".table-responsive")
+                .$(byText("State and City"))
+                .parent()
+                .shouldHave(text("NCR Noida"));
     }
 }
